fb9043b10addbebbc6d6242526ed6da13c7afe70
Petar Petrovic <petarpetrovic@primer.com>
2025-04-04 16:44:47 +0200

    WWWA

--- src/komponente/RegisterPage.jsx
+++ src/komponente/RegisterPage.jsx
@@ -26,7 +26,7 @@ function RegisterPage() {
              
             e.preventDefault();   
             axios
-                .post("http://0.0.0.0:8080/api/register", userData, {
+                .post("https://mojcv-production-8561.up.railway.app/api/register", userData, {
                 headers: {
                     "Content-Type": "application/json"
                 }

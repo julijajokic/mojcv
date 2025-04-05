eda4a463ceee716e881ddb0503055bb4751bb794
Petar Petrovic <petarpetrovic@primer.com>
2025-04-05 11:16:49 +0200

    REW

--- api/vite.config.js
+++ api/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         host: '0.0.0.0',  // Omogućava pristup sa drugih uređaja
         port: 9000,       // Postavlja port na 9000
         proxy: {
-            '/api': 'http://0.0.0.0:8080',  // Proxy za API pozive ka Laravel serveru
+            '/api': 'https://mojcv-production-8561.up.railway.app/api'
         },
     },
 });

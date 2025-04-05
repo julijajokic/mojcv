dc41a376795f509cf9233f885e3fdacb4a2d7869
Petar Petrovic <petarpetrovic@primer.com>
2025-04-05 22:25:22 +0200

    JYT

--- src/komponente/LoginPage.jsx
+++ src/komponente/LoginPage.jsx
@@ -51,31 +51,6 @@ function LoginPage() {
     }
 
     return (
-        <div>
-            <form onSubmit={handleLogin}>
-                <input
-                    type="name"
-                    name="name"
-                    placeholder="Name"
-                    value={userData.name}
-                    onChange={handleInput}
-                />
-                <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={userData.email}
-                    onChange={handleInput}
-                />
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    );
-}
-
-
-
-    return (
         <div className='login'>
             <div className="page-wrapper bg-gra-01 p-t-180 p-b-100 font-poppins">
                 <div className="wrapper wrapper--w780">
@@ -83,42 +58,36 @@ function LoginPage() {
                         <div className="card-heading"></div>
                         <div className="card-body">
                             <h2 className="title">Log in</h2>
-                            <form onSubmit={handleLogin}>
+                            <form onSubmit={handleLogin} >                          
+                                                      
                                 <div className="input-group">
                                     <input 
                                         className="input--style-3" 
-                                        type="name"  // Ispravljeno: koristi "email" umesto "text"
+                                        type="name" 
                                         placeholder="Name" 
                                         name="name"
-                                        value={userData.name}
-                                        onChange={handleInput} 
+                                        onInput={handleInput}
                                     />
                                 </div>
                                 
                                 <div className="input-group">
-                                    <input 
-                                        className="input--style-3" 
-                                        type="email"  // Dodano polje za lozinku
-                                        placeholder="Email" 
-                                        name="email"
-                                        value={userData.email}
-                                        onChange={handleInput} 
-                                    />
+                                    <input className="input--style-3" type="email" placeholder="Email" name="email"  onInput={handleInput}/>
                                 </div>
-
                                 <div className="p-t-10">
                                     <button className="btn btn--pill btn--green" type="submit" id="login" name="login">Submit</button>
                                 </div>
-                                <br /><br />
-                                <p><a href="/register" className='tekstForme'>I am new here!</a></p>
-                            </form>
-                        </div>
+                                <br/><br/>
+                                <p><a href="/register"  className='tekstForme'>I am new here!</a></p>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
+            </div>
+        
     );
+    
 
-
+    }
 export default LoginPage;
 

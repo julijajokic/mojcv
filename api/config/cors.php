e30c9ebeb7acc4df64912faadcc665a385fbb746
Petar Petrovic <petarpetrovic@primer.com>
2025-04-06 13:35:27 +0200

    LKO

--- api/config/cors.php
+++ api/config/cors.php
@@ -5,7 +5,7 @@ return [
     
     'allowed_methods' => ['*'],  // Dozvoljava sve HTTP metode (GET, POST, PUT, DELETE).
     
-    'allowed_origins' => ['*'],  // Ovde stavite URL vašeg frontend-a.
+    'allowed_origins' => ['https://mojcv-production-8561.up.railway.app'],  // Ovde stavite URL vašeg frontend-a.
     
     'allowed_origins_patterns' => [],
 

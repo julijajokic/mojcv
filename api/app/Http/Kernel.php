b9b9ebeb24c42d55a232438e3f62589f63e75c6d
Petar Petrovic <petarpetrovic@primer.com>
2025-04-06 19:21:26 +0200

    VRT

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -42,12 +42,16 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
         
-        'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
-    ];
+   
+       
+        
+            'api' => [
+                \Illuminate\Cors\HandleCors::class, // Ovdje dodaj Cors middleware
+                'throttle:api',
+                \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            ],
+        ];
+        
 
     
     // 'api' => [

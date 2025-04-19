907dc06158983a6003cec46780ef0880f59bb30a
Petar Petrovic <petarpetrovic@primer.com>
2025-04-19 13:00:43 +0200

    PLOK

--- api/config/cors.php
+++ api/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://mojcv-production.up.railway.app', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 

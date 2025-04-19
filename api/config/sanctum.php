0c9d92e4f5a8b9d1628b2bc7df8d8c4ad32898fa
Petar Petrovic <petarpetrovic@primer.com>
2025-04-19 12:27:37 +0200

    plof

--- api/config/sanctum.php
+++ api/config/sanctum.php
@@ -15,11 +15,13 @@ return [
     |
     */
 
+
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'mojcv-production-8561.up.railway.app,mojcv-production-8561.up.railway.app:9000,mojcv-production-8561.up.railway.app,mojcv-production-8561.up.railway.app:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
+
 
     /*
     |--------------------------------------------------------------------------

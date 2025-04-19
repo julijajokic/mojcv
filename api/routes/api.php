90421b73631ca090275137b0496821fe12cef312
Petar Petrovic <petarpetrovic@primer.com>
2025-04-19 14:18:53 +0200

    HGYU

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,16 +4,30 @@ use App\Http\Controllers\AutfController;
  use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post("/register", [AutfController::class, 'register']);
-Route::post("/login",[AutfController::class,'login']);
-Route::post("/logout",[AutfController::class,'logout']);
+// Route::post("/register", [AutfController::class, 'register']);
+// Route::post("/login",[AutfController::class,'login']);
+// Route::post("/logout",[AutfController::class,'logout']);
 
-Route::get('/status', function () {
-    return response()->json(['status' => 'OK']);
+// Route::get('/status', function () {
+//     return response()->json(['status' => 'OK']);
+// });
+// Route::get('/test', function () {
+//     return response()->json(['msg' => 'API je živ']);
+// });
+Route::middleware('force_json')->group(function () {
+    Route::post("/register", [AutfController::class, 'register']);
+    Route::post("/login", [AutfController::class, 'login']);
+    Route::post("/logout", [AutfController::class, 'logout']);
+
+    Route::get('/status', function () {
+        return response()->json(['status' => 'OK']);
+    });
+
+    Route::get('/test', function () {
+        return response()->json(['msg' => 'API je živ']);
+    });
 });
-Route::get('/test', function () {
-    return response()->json(['msg' => 'API je živ']);
-});
+
 
 
 
